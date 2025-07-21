be4ac85909908c02321892c00421ed24bf7a4685
hlnkien12 <141345978+hlnkien12@users.noreply.github.com>
2025-07-21 10:51:12 +0700

    Update AuthenticationFilter.java
    Tạo lại và update code lại thành công

--- AuthenticationDemo/src/java/filter/AuthenticationFilter.java
+++ AuthenticationDemo/src/java/filter/AuthenticationFilter.java
@@ -7,5 +7,18 @@ import java.io.IOException;
 
 @WebFilter("/secure/*")
 public class AuthenticationFilter implements Filter {
-    
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        HttpSession session = req.getSession(false);
+        Object user = (session != null) ? session.getAttribute("loggedinUser") : null;
+
+        if (user == null) {
+            res.sendRedirect(req.getContextPath() + "/login.jsp");
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
 }
